8c13a0451f03c7fdb32f1713aac37f5ee0a5062c
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-04-26 11:40:19 -0300

    Refactor: model da rota get/orders

--- src/models/OrderModel.ts
+++ src/models/OrderModel.ts
@@ -2,13 +2,13 @@ import { RowDataPacket } from 'mysql2';
 import connection from './connection';
 
 async function getAll(): Promise<RowDataPacket[]> {
-  const [listOfServices] = await connection.execute<RowDataPacket[]>(`
-    SELECT o.id,o.user_id as userId, json_arrayagg(p.id) as productsIds
-    FROM Trybesmith.orders as o
-    INNER JOIN Trybesmith.products as p
-    ON o.id = p.order_id   
-    GROUP BY o.id`);
-  
+  const query = `SELECT o.id,o.user_id as userId, json_arrayagg(p.id) as productsIds
+  FROM Trybesmith.orders as o
+  INNER JOIN Trybesmith.products as p
+  ON o.id = p.order_id   
+  GROUP BY o.id`;
+
+  const [listOfServices] = await connection.execute<RowDataPacket[]>(query);
   return listOfServices;
 }
 
